8ec3f79f0f9aee1f28a8ac5611401ae3058a7527
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-09 16:43:42 +0100

    ho creato la stessa struttura di prima

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -4,6 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+//! Riprendo la stessa idea dell'esercizio 1
+
+//** Inanzitutto devo creare una funzione che prenda un array di nome.
+// ** La chiamerò inizialeParole che sarà una const perché non cambia.
+//** dentro questa costante metterò una funzione */
+//**farò diventare inizialeParole il nome della funzione */
+//** userò la forma arrow perché mi sembra più facile */
+
+//stampo la lista 
+console.log(names);
+
+
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
