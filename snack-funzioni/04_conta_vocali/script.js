65e3d24104da6c0a7ed4d5ad3c9d1c0e02c09022
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-13 22:58:13 +0100

    ho creato una arrow function

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,11 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const countVocali =(text) => {
+
 const vocali = ["a", "e", "i", "o", "u"];
 let vocaliCounter = 0;
 
-for (let i = 0; i < text.length; i++);
+for (let i = 0; i < text.length; i++); {
+const letteraCorrente = text[i];
 
+if (vocali.includes(letteraCorrente)) vocaliCounter++;
+    
+}
+}
 // Invoca la funzione qui e stampa il risultato in console
 
 
